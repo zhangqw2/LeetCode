7db23f9dc1595b345a6c2ba93c5c8568e6f0cd16
zhangqw2 <43216619+zhangqw2@users.noreply.github.com>
2021-06-28 14:13:47 +0800

    Create 合并两个有序链表.java

---
+++ 合并两个有序链表.java
@@ -0,0 +1,23 @@
+/**
+*将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+*/
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+
+    }
+}
